c13bd1447e5126538d5945042d9486fc641f4f6b
Edward Edberg <edward.edberg.halim@gmail.com>
2022-01-16 23:26:05 -0800

    homepage content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Welcome to IAIF</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quam
+        fuga suscipit magni, saepe iure aut assumenda at laborum placeat, enim
+        sint. Exercitationem corporis atque pariatur dolorem repellat itaque
+        enim cum ad neque assumenda numquam aut quisquam eaque doloremque,
+        beatae quaerat cupiditate cumque, tenetur eligendi dignissimos sapiente
+        consequuntur. Totam, illo!
+      </p>
+      <h2>Vision</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, unde!
+      </p>
+      <h2>Mission</h2>
+      <ul className="item-list">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <footer>
+        <p>Copyright &copy; {new Date().getFullYear()} by IAIF</p>
+      </footer>
     </div>
   );
 }
